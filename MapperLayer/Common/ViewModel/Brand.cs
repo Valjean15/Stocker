af3ad52a5f78177936cd17c8649f43304d837d2c
Valjean3945 <jeanlan3945@hotmail.com>
2019-11-03 09:52:57 -0600

    -  Limpieza en repositorio

---
+++ MapperLayer/Common/ViewModel/Brand.cs
@@ -0,0 +1,30 @@
+﻿namespace Mapper.Common.ViewModel
+{
+    /// <summary>
+    ///     ViewModel - Marca de producto
+    /// </summary>
+    public class Brand : Base.IViewModel<Models.Common.Brand>
+    {
+        /// <summary>
+        ///     Id de la marca
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        ///     Nombre de la marca
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        ///     Map del viewmodel a entidad correspondiente
+        /// </summary>
+        public Models.Common.Brand ToModel()
+        {
+            return new Models.Common.Brand
+            {
+                Id = Id,
+                Name = Name
+            };
+        }
+    }
+}
